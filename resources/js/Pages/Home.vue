e17a50e990d6fef6f93dff987890f2fca94fd7ee
SiddharthPant <spsiddharthpant@gmail.com>
2024-07-28 18:59:39 +0530

    Create a basic home page using inertia render

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array,
+    }
+}
+</script>
+
+<template>
+    <h1>Hello, {{ name }}</h1>
+
+    <p>In this series, we will use the following frameworks:</p>
+
+    <ul>
+        <li v-for="framework in frameworks" v-text="framework"></li>
+    </ul>
+</template>
