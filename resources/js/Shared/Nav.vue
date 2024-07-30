8f9cce06548a279f0b7ebc765ea56459c0d2ee0e
SiddharthPant <spsiddharthpant@gmail.com>
2024-07-30 12:41:16 +0530

    Logout link and fix some css stuff

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -8,11 +8,12 @@ export default {
 </script>
 
 <template>
-    <nav>
-        <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/users">Users</Link></li>
-            <li><Link href="/settings">Settings</Link></li>
+    <nav class="mt-6">
+        <ul class="list-disc">
+            <li><Link href="/" class="text-blue-500 hover:underline">Home</Link></li>
+            <li><Link href="/users" class="text-blue-500 hover:underline" >Users</Link></li>
+            <li><Link href="/settings" class="text-blue-500 hover:underline" >Settings</Link></li>
+            <li><Link href="/lgout" class="text-blue-500 hover:underline"  method="post" as="button">Log Out</Link></li>
         </ul>
     </nav>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,19 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
 Route::get('/users', function () {
     sleep(2);
 
     return Inertia::render('Users');
 });
+
 Route::get('/settings', function () {
     return Inertia::render('Settings');
+});
+
+Route::post('/lgout', function () {
+    dd('logging the user out');
 });
 
 Route::get('/dashboard', function () {
